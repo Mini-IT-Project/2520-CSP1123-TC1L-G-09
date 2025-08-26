3b310d9691270620b52d6aeb51cc78ec5aca1ef9
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 03:50:47 +0800

    debug3

--- src/bottle_feature.py
+++ src/bottle_feature.py
@@ -1,43 +1,27 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from extensions import db 
 
 bottle_bp = Blueprint("bottle", __name__)
 
-#Store drifting bottle
-bottles = []
-
 class Bottle(db.Model):
-    __tablename__ = 'bottles_table'
-    id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.Text,nullable=False)
-    campus = db.Column(db.String(50),nullable=False,default=cyberjaya)
-    status = db.Column(db.String(20),default='unpicked')
-    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False)
 
-    def __repr__(self):
-        return f"Bottle{self.id} - {self.status} - {self.campus}"
-    
-@bottle_bp.route("/throw", methods=["POST"])
-def throw():
-    content = request.form.get("content")
-    campus = request.form.get("campus")
-    if content and campus:
-        new_bottle = Bottle(content=content, campus=campus)
-        db.session.add(new_bottle)
+@bottle_bp.route("/throw", methods=["POST"], endpoint="throw")
+def throw_bottle():
+    msg = request.form.get("message")
+    if msg:
+        bottle = Bottle(message=msg)
+        db.session.add(bottle)
         db.session.commit()
     return redirect(url_for("main.index"))
 
-@bottle_bp.route("/pick")
-def pick():
-    campus = request.args.get("campus", "all")
-    query = Bottle.query.filter_by(status="unpicked")
-    if campus != "all":
-        query = query.filter_by(campus=campus)
-    bottles = query.all()
-    
-    if bottles:
-        bottle = random.choice(bottles)
-        bottle.status = "picked"
+@bottle_bp.route("/pick", endpoint="pick")
+def pick_bottle():
+    bottle = Bottle.query.first()
+    if bottle:
+        msg = bottle.message
+        db.session.delete(bottle)
         db.session.commit()
-        return render_template("pick.html", bottle=bottle)
-    else:
-        return render_template("pick.html", bottle=None)
+        return render_template("pick.html", message=msg)
+    return render_template("pick.html", message="There is nothing...")
