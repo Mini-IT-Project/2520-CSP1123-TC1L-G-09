6ba4ea19fad79121a3d42db3ff388a9301f362da
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 03:52:39 +0800

    debug4

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bottles.db'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = init_app(app)
+db.init_app(app)
 socketio.init_app(app)
 
 app.register_blueprint(main_bp, url_prefix="/")
