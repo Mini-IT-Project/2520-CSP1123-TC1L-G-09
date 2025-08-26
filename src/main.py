4942702db1b6864a515aae4cc8361c451997bbc8
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 02:43:22 +0800

    trying blueprint

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-from flask import Blueprint, render_template
-from bottle import Bottle   
-from bottle import db       
+from flask import Blueprint, render_template    
 
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def index():
-    unpicked_count = Bottle.query.filter_by(status="unpicked").count()
-    return render_template("index.html", unpicked_count=unpicked_count)
+    return render_template("index.html")
