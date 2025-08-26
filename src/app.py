b6621eb75f169dd55c21bc78d975889c3ec7cdee
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 03:50:07 +0800

    debug2

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from main import main_bp
 from bottle_feature import bottle_bp
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bottles.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = init_app(app)
+socketio.init_app(app)
 
 app.register_blueprint(main_bp, url_prefix="/")
 app.register_blueprint(bottle_bp, url_prefix="/bottle")
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app,debug=True)
