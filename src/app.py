27c3eed9f62dcdda9b15f5479f2110888c6a0929
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 04:03:34 +0800

    trying......

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from main import main_bp
 from bottle_feature import bottle_bp
+from extensions import db, socketio  # ✅ 从 extensions 导入
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bottles.db'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bottles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -17,4 +17,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    socketio.run(app,debug=True)
+    socketio.run(app, debug=True)
--- src/extensions.py
+++ src/extensions.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-from extensions import db, socketio
 
 db = SQLAlchemy()
 socketio = SocketIO(async_code="eventlet", cors_allowed_origins="*")
