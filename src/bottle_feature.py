c82fa12984771bd1d56e7579aa830695b1d9a30b
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-27 03:14:43 +0800

    try filter campus

--- src/bottle_feature.py
+++ src/bottle_feature.py
@@ -5,14 +5,25 @@ bottle_bp = Blueprint("bottle", __name__)
 #Store drifting bottle
 bottles = []
 
-@bottle_bp.route("/throw", methods=["POST"])
+class Bottle(db.Model):
+    __tablename__ = 'bottles_table'
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.Text,nullable=False)
+    campus = db.Column(db.String(50),nullable=False,default=cyberjaya)
+    status = db.Column(db.String(20),default='unpicked')
+    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Bottle{self.id} - {self.status} - {self.campus}"
+    
+@bottle_bp.route("/throw", methods=["POST"],endpoint="throw")
 def throw_bottle():
     message = request.form.get("message")
     if message:
         bottles.append(message)
     return redirect(url_for("main.index"))
 
-@bottle_bp.route("/pick")
+@bottle_bp.route("/pick",endpoint="pick")
 def pick_bottle():
     if bottles:
         picked = bottles.pop(0)   
