92c7a09cf3bf9bf85e9cf1773ead029f8d7721d3
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-28 22:35:37 +0800

    debug

--- src/app.py
+++ src/app.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from main import main_bp
 from bottle_feature import bottle_bp
-from extensions import db, socketio  # ✅ 从 extensions 导入
+from extensions import db, socketio  
+import os
 
 app = Flask(__name__)
+
+#database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bottles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#upload file
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "static", "uploads")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 db.init_app(app)
-socketio.init_app(app)
 
 app.register_blueprint(main_bp, url_prefix="/")
 app.register_blueprint(bottle_bp, url_prefix="/bottle")
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
