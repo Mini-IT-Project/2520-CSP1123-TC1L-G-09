0cb2e3c3e9a9cdd5b05bca19fc90d3dea72f7c21
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-29 23:21:45 +0800

    adjustment

--- src/app.py
+++ src/app.py
@@ -33,8 +33,8 @@ def create_app():
     return app
 
 #website address
-app = create_app()
 if __name__ == "__main__":
+    app = create_app()
     socketio.run(
         app,
         host='127.0.0.1',
