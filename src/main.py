39e1468705177b5d19684d28e145426c4ee1a3c5
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-30 21:17:10 +0800

    adjustment

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
-from flask import Blueprint, render_template    
+from flask import Blueprint, render_template   
 
 main_bp = Blueprint("main", __name__)
 
-events = [
-    {"title":"Orientation Week 2025","date":"2025-09-05","description":"Welcome to MMMU!"}
-]
-
-announcements = [
-    {"title":"New Shuttle Bus Route","date":"2025-09-03","description":"New route added from Cyberjaya to Putrajaya Sentral!"}
-]
-
 @main_bp.route("/")
-def homepage():
-    return render_template("homepage.html",events=events, announcements=announcements)
+def index():
+    return render_template("index.html", events=events, announcements=announcements)
