7ac58ec1eba7e74bcfa03b6c667b3669993a69e5
LIM XING HOOI <xinghooilim0123@gmail.com>
2025-08-30 20:37:10 +0800

    trying basic homepage ing...

--- src/main.py
+++ src/main.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
 
+events = [
+    {"title":"Orientation Week 2025","date":"2025-09-05","description":"Welcome to MMMU!"}
+]
+
+announcements = [
+    {"title":"New Shuttle Bus Route","date":"2025-09-03","description":"New route added from Cyberjaya to Putrajaya Sentral!"}
+]
+
 @main_bp.route("/")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("homepage.html",events=events, announcements=announcements)
