8573c4a0e2303e63adc2b1280dd2ba4c2b1c4599
Zhengsiew <carinakoh0904@gmail.com>
2025-09-07 01:51:58 +0800

    correction

--- src/forum_routes.py
+++ src/forum_routes.py
@@ -18,7 +18,7 @@ def homepage():
     return render_template("forum_home.html")
 
 
-ALLOWED_EXT = {"png","jpg","jpeg","git","mp4","mov"}
+ALLOWED_EXT = {"png","jpg","jpeg","gif","mp4","mov"}
 
 def allowed_file(filename: str) -> bool:  #check the filename
     if "." not in filename:
@@ -101,7 +101,7 @@ def create_post():
     socketio.emit("new_post",new_post.to_dict(base_url=request.host_url))
 
     flash("Post Uploaded!","success")
-    return redirect(url_for("forum.forum_homepage"))
+    return redirect(url_for("forum.forum_home"))
 
 def handle_file_upload(file_object):
     if not file_object or not allowed_file(file_object.filename):
