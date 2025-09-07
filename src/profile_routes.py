4bb8fa456e50528ed01ff0199d441dd409cfbf7a
Jy-Jimmy <jiayonglim1226@gmail.com>
2025-09-07 23:15:50 +0800

    classify profile

--- src/profile_routes.py
+++ src/profile_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,session,redirect,url_for
+from flask import Blueprint,render_template,session,redirect,url_for, flash
 from forum_models import Comment,Post,Like
 from login import Users
 
@@ -11,14 +11,43 @@ profile_bp = Blueprint(
 )
 
 @profile_bp.route("/")
-def profile():
+def myProfile():
     user_id = session.get("user_id")
     if not user_id:
+        flash("Please login in first.")
         return redirect(url_for('login.home'))
     
     user = Users.query.get(user_id) 
     if not user:
+        flash("Please login in first.")
+        return redirect(url_for('login.home'))
+    
+    return render_template("myProfile.html")
+
+@profile_bp.route("/history")
+def profile():
+    user_id = session.get("user_id")
+    if not user_id:
+        flash("Please login in first.")
+        return redirect(url_for('login.home'))
+    
+    user = Users.query.get(user_id) 
+    if not user:
+        flash("Please login in first.")
         return redirect(url_for('login.home'))
         
     return render_template("profile.html", user=user)
 
+@profile_bp.route("/Settings")
+def settings():
+    user_id = session.get("user_id")
+    if not user_id:
+        flash("Please login in first.")
+        return redirect(url_for('login.home'))
+    
+    user = Users.query.get(user_id) 
+    if not user:
+        flash("Please login in first.")
+        return redirect(url_for('login.home'))
+    
+    return render_template("settings.html")
